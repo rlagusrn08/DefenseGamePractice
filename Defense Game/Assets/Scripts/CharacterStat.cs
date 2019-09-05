57679306436d0fb6904e3544109f372d4b29e892
rlagusrn08 <rlagusrn08@gmail.com>
2019-09-05 23:27:15 +0900

    초기 프로젝트 설정

---
+++ Defense Game/Assets/Scripts/CharacterStat.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStat : MonoBehaviour
+{
+    public int level = 1; //캐릭 레벨
+    public int hp = 30; // 현재 체력
+    public int maxHp = 30; // 최대 체력
+    public int damage = 5;
+    public int cost = 130; // 캐릭터 가격
+    public int upgradeCost = 200; //업그레이드 가격
+    public float coolTime = 2.0f;
+
+    private Animator animator;
+
+    public int attacked(int damage)
+    {
+        hp = hp - damage;
+        if(hp<=0)
+        {
+            animator.SetTrigger("Die");
+            Destroy(gameObject, 1.5f);
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+        return hp;
+    }
+    public bool canCreate(int seed)
+    {
+        if(cost <= seed)
+        { return true;}
+        return false;
+    }
+
+    //레벨업이 가능한지의 여부
+    public bool canLevelUp(int seed)
+    {
+        if(level<3)
+        {
+            if(upgradeCost <= seed)
+            {
+                return true;
+            }
+            return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void increaseLevel()
+    {
+        if(level == 1)
+        {
+            level = 2;
+            maxHp += 25;
+            hp = maxHp;
+            damage += 5;
+            transform.localScale += new Vector3(0.01f, 0.01f, 0);
+        }
+        else if(level == 2)
+        {
+            level = 3;
+            maxHp += 50;
+            hp = maxHp;
+            damage += 5;
+            transform.localScale += new Vector3(0.01f, 0.01f, 0);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Defense Game/Assets/Scripts/StartManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Advertisements;
+
+public class StartManager : MonoBehaviour
+{
+    public void GameStart()
+    { 
+        SceneManager.LoadScene("GameScene");
+        Time.timeScale = 1;
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowRewardedAd();
+        Screen.SetResolution(1920, 1200, true);
+        Advertisement.Initialize("3279749", false);
+    }
+
+    private void ShowRewardedAd()
+    {
+        if(Advertisement.IsReady("rewardedVideo"))
+        {
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show("rewardedVideo", options);
+        }
+    }
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("사용자가 광고를 성공적으로 보았음");
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("사용자가 광고를 스킵함");
+                break;
+            case ShowResult.Failed:
+                Debug.Log("광고에서 오류 발생");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
